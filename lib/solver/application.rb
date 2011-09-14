982def33ada51ea63afc903182d31bbebcce7e8f
Gregory Parkhurst <six.impossible@gmail.com>
2011-09-14 14:26:54 -0500

    minor refactor

--- lib/solver/application.rb
+++ lib/solver/application.rb
@@ -1,43 +1,43 @@
 module CircuitSimulator
   class Application
     def self.run(argv)
-      @options = {}
-      get_options
+      options = {}
+      get_options(options).parse!
 
-      s = Solver.new(@options)
+      s = Solver.new(options)
       s.solve
       puts s.outputs
 
-      s.truth_table if @options[:truth]
+      s.truth_table if options[:truth]
     end
 
-    def self.get_options
+    def self.get_options(options)
       OptionParser.new do |opts|
         opts.banner = "Usage: #{$PROGRAM_NAME} [options] FILE [...]"
 
         opts.on("-c", "--circuit CIRCUIT", "Name of top-level circuit") do |c|
-          @options[:circuit] = c
+          options[:circuit] = c
         end
 
         opts.on("-i", "--inputs \"A:1, B:0, [...]\"", "Specify inputs") do |i|
           list = i.split(/[, ]+/)
-          @options[:inputs] = {}
+          options[:inputs] = {}
 
           list.each do |i|
             k,v = i.split(':')
-            @options[:inputs][k] = (v.to_i == 1 ? true : false)
+            options[:inputs][k] = (v.to_i == 1 ? true : false)
           end
         end
 
         opts.on("-t", "--truth", "Compute truth table") do |t|
-          @options[:truth] = t
+          options[:truth] = t
         end
 
         opts.on("-v", "--verbose", "Run verbosely") do |v|
-          @options[:verbose] = v
+          options[:verbose] = v
         end
 
-      end.parse!
+      end
     end
   end
 end
